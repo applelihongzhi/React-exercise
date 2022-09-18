f83c441b9c8a152e3c60dafd770c7e3776b0e841
lihongzhi_xc <1304395379@qq.com>
2022-09-18 16:33:07 +0800

    kanban

---
+++ my-kanban-app/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './components/task.css';
+import './components/event.css';
+import './App.css'
+import React from 'react';
+import EventBar from './components/EventBar';
+import TaskBox from './components/TaskBox';
+function App() {
+  const initEvent = [
+    {
+      title: 'Add a new Event',
+      ['To do']: [],
+      ['In progress']: [],
+      ['Completed']: [],
+    },
+  ];
+  const [events,setEvents] = React.useState(()=>{
+    return localStorage.getItem('events')
+    ? JSON.parse(localStorage.getItem('events'))
+    : initEvent;
+  })
+  const [currentEvent, setCurrentEvent] = React.useState(events[0]);
+    // Set localStorage
+    React.useEffect(() => {
+      if (!events.length) {
+        localStorage.setItem('events', JSON.stringify(initEvent));
+        setEvents(JSON.parse(localStorage.getItem('events')));
+      } else {
+        localStorage.setItem('events', JSON.stringify(events));
+      }
+    }, [events]);
+  return (
+    <div className="App">
+      <EventBar
+         events={events}
+        setEvents={setEvents}
+        currentEvent={currentEvent}
+        setCurrentEvent={setCurrentEvent}
+      />
+     <TaskBox
+        events={events}
+        setEvents={setEvents}
+        currentEvent={currentEvent}
+        setCurrentEvent={setCurrentEvent}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ my-kanban-app/src/components/AddTaskButton.jsx
@@ -0,0 +1,10 @@
+const AddTaskButton = ({ handleClick }) => {
+    return (
+      <div className='add-task-button' onClick={handleClick}>
+        +
+      </div>
+    );
+  };
+  
+  export default AddTaskButton;
+  
